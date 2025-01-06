4c571f73ef5400b494ca80b00342c7ccafe02107
StrudyGrass <2804597126@qq.com>
2025-01-06 13:36:44 +0800

    创建dev分支，并推送到github

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("创建dev分支，并推送到github");
+        System.out.println("完成一部分内容后，dev分支先提交然后推github");
+        System.out.println("然后到master分支上，去合并一下分支，然后推master分支到github上");
     }
+
 }
