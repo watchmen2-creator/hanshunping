7e6b744ddf3503b8b24b544b791d78d0effbf192
StrudyGrass <2804597126@qq.com>
2025-04-01 14:13:59 +0800

    第 10 章 、 第 11 章的作业放在一块了，第7个问题：汽车内空调吹风的

---
+++ src/chapter10/com/hspedu/homework/Homework07.java
@@ -0,0 +1,12 @@
+package chapter10.com.hspedu.homework;/*
+ * @author  SturdyGrass(working)
+ * @version 1.0
+ * 设置这个代码文件模板的头部信息的路径在这：
+ * File -> Settings -> Editor -> File and Code Templates -> Includes -> File Header
+ * */
+
+public class Homework07 {
+
+}
+
+//Car类，有属性tem
