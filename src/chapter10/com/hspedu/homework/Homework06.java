5aa3f297b2dcbda71f992506f4d6039e9337a712
StrudyGrass <2804597126@qq.com>
2025-04-01 13:59:45 +0800

    第 10 章 、 第 11 章的作业放在一块了，唐僧与交通工具的问题结束

--- src/chapter10/com/hspedu/homework/Homework06.java
+++ src/chapter10/com/hspedu/homework/Homework06.java
@@ -7,7 +7,12 @@ package chapter10.com.hspedu.homework;/*
 
 public class Homework06 {
     public static void main(String[] args) {
-
+        Person tang = new Person("唐僧", new Horse());
+        tang.common();
+        tang.passRiver();
+        tang.common();
+        tang.passRiver();
+        tang.passFlamesMounTain();
     }
 }
 
@@ -29,19 +34,31 @@ class Boat implements IVehicles {
     }
 }
 
+class Plane implements IVehicles {
+    @Override
+    public void work() {
+        System.out.println("遇到火焰山就坐飞机");
+    }
+}
+
 class VehiclesFactory {
+    //马始终是同一匹马
+    private static Horse horse = new Horse();//饿汉式单例模式，在类加载时就创建对象，避免频繁创建对象
+
     public IVehicles getVehicle(String type) {
         if (type.equals("Horse")) {
-            return new Horse();
+            return horse;
         } else if (type.equals("Boat")) {
             return new Boat();
+        } else if (type.equals("Plane")) {
+            return new Plane();
         }
         return null;
     }
 
     //工厂就创建一下对象，不用工厂的其他东西，所以可以不创建工厂的对象，把这两个方法做成静态方法
     public static Horse getHorse() {
-        return new Horse();
+        return horse;
     }
 
     //工厂就创建一下对象，不用工厂的其他东西，所以可以不创建工厂的对象，把这两个方法做成静态方法
@@ -49,22 +66,53 @@ class VehiclesFactory {
         return new Boat();
     }
 
+    //工厂就创建一下对象，不用工厂的其他东西，所以可以不创建工厂的对象，把这两个方法做成静态方法
+    public static Plane getPlane() {
+        return new Plane();
+    }
+
 }
 
 class Person {
+
     private String name;
     private IVehicles Vehicle;
 
-    public Person() {
-        setName("唐僧");
-        setVehicle(new Horse());
-    }
+//    public Person() {
+//        setName("唐僧");
+//        setVehicle(new Horse());
+//    }
 
     public Person(String name, IVehicles vehicle) {
         setName(name);
         setVehicle(vehicle);
     }
 
+    //遇到河要坐船
+    public void passRiver() {
+        if (!(Vehicle instanceof Boat)) {
+            this.setVehicle(VehiclesFactory.getBoat());
+        }
+        Vehicle.work();
+    }
+
+    //一般情况骑马
+    public void common() {
+        //为了避免创建对象时传入的交通工具浪费，所以在这里判断一下是不是有交通工具了
+        if (!(Vehicle instanceof Horse)) {//如果现在的交通工具不是马，就去工厂获得一个马
+            this.setVehicle(VehiclesFactory.getHorse());
+        }
+        Vehicle.work();
+
+    }
+    //火焰山坐飞机
+    public void passFlamesMounTain(){
+        //为了避免创建对象时传入的交通工具浪费，所以在这里判断一下是不是有交通工具了
+        if (!(Vehicle instanceof Plane)) {//如果现在的交通工具不是马，就去工厂获得一个马
+            this.setVehicle(VehiclesFactory.getPlane());
+        }
+        Vehicle.work();
+    }
 
     public String getName() {
         return name;
