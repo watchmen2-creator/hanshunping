476cfb5284cc7d4f00cf3c5a5301308863584917
StrudyGrass <2804597126@qq.com>
2025-04-02 13:23:09 +0800

    第 10 章 、 第 11 章的作业放在一块了，第7个问题：汽车内空调吹风的

--- src/chapter10/com/hspedu/homework/Homework07.java
+++ src/chapter10/com/hspedu/homework/Homework07.java
@@ -6,7 +6,44 @@ package chapter10.com.hspedu.homework;/*
  * */
 
 public class Homework07 {
+    public static void main(String[] args) {
+        Car2 car = new Car2(25);
+        car.getAir().flow();
+        Car2 car2 = new Car2(-5);
+        car2.getAir().flow();
+        Car2 car3 = new Car2(60);
+        car3.getAir().flow();
+    }
 
 }
 
-//Car类，有属性tem
+//Car类，有属性温度
+
+class Car2 {
+    private double temperature;//温度属性
+
+    //构造器
+
+
+    public Car2(double temperature) {
+        this.temperature = temperature;
+    }
+
+    //成员内部类：空调
+    class Air {
+
+        //吹风功能
+        public void flow() {
+            if (temperature > 40) {
+                System.out.println("吹冷风");
+            } else if (temperature < 0) {
+                System.out.println("吹暖风");
+            }
+        }
+    }
+
+    //方案返回Air对象
+    public Air getAir() {
+        return new Air();
+    }
+}
