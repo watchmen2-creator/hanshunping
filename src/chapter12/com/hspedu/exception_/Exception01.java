0c15a1366908d15bce3ee045eed9032881f3ad82
StrudyGrass <2804597126@qq.com>
2025-04-02 14:03:52 +0800

    第12章，异常入门

---
+++ src/chapter12/com/hspedu/exception_/Exception01.java
@@ -0,0 +1,15 @@
+package chapter12.com.hspedu.exception_;/*
+ * @author  SturdyGrass(working)
+ * @version 1.0
+ * 设置这个代码文件模板的头部信息的路径在这：
+ * File -> Settings -> Editor -> File and Code Templates -> Includes -> File Header
+ * */
+
+public class Exception01 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 0;
+
+
+    }
+}
